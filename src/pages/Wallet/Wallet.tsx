9790e1ecfef2226503f0e9ead7e66fe99a76eac0
RexHung <zehung860486@gmail.com>
2025-05-16 16:14:42 +0800

    feat: add redux

--- src/pages/Wallet/Wallet.tsx
+++ src/pages/Wallet/Wallet.tsx
@@ -1,6 +1,7 @@
 import WalletList from "../../components/WalletList/WalletList";
 import useWalletController from "./useWalletController";
 import styles from './wallet.module.scss';
+
 /**
  * @description Wallet page
  * @returns {JSX.Element}
